13460ecb29ad7b8f85b96d7394e70f2eebd55038
Martin Giger <martin.a.giger@sunrise.ch>
2015-04-09 01:05:00 +0200

    Get rid of unsafe-inline script sources

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,3 +2,16 @@ if(Mines.hasSavedState()) {
     document.getElementById("resumeSection").classList.remove("hidden");
     document.getElementById("resumeSection").removeAttribute("aria-hidden");
 }
+
+var buttons = document.querySelectorAll(".gaia-list button");
+for(var b = 0; b < buttons.length; ++b) {
+    buttons[b].addEventListener("click", function(e) {
+        if("custom" == e.target.dataset.difficulty) {
+            window.location = "custom.html";
+        }
+        else {
+            window.location = "mines.html#"+e.target.dataset.difficulty;
+        }
+    });
+}
+
