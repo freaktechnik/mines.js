ec72bea4e65f9c41c7fa4aa421d2c67f3b3461f0
Martin Giger <martin.a.giger@sunrise.ch>
2015-04-10 16:50:35 +0200

    Display correct vibration setting state

---
+++ assets/scripts/settings.js
@@ -0,0 +1,23 @@
+document.getElementById("vibration").checked = localStorage.getItem("vibration") == "enabled";
+
+document.getElementById("vibration").addEventListener("change", function(e) {
+    if(e.target.checked) {
+        localStorage.setItem("vibration", "enabled");
+    }
+    else {
+        localStorage.setItem("vibration", "disabled");
+    }
+});
+
+document.querySelector("[data-l10n-id='settings_highscores_clear']").addEventListener("click", function() {
+    if(window.confirm(document.querySelector("[data-l10n-id='highscores_confirm_clear']").textContent)) {
+        Highscores.clear();
+        removeDynamicItems();
+        noresults.classList.remove("hidden");
+    }
+});
+
+document.getElementById("header").addEventListener("action", function() {
+    window.location = "index.html";
+}, false);
+
