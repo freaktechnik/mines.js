1fc2427abfe0601e27ef6a83546c4cef9f37898c
freaktechnik <martin.a.giger@sunrise.ch>
2015-05-01 21:36:01 +0200

    modularize settings. Kind of.

--- assets/scripts/settings.js
+++ assets/scripts/settings.js
@@ -1,27 +1,14 @@
-BoolPreference.TRUE = "enabled";
-BoolPreference.FALSE = "disabled";
-BoolPreference.prototype.value = false;
-function BoolPreference(name, defaultValue) {
-    var pref = localStorage.getItem(name);
-    if(pref != BoolPreference.TRUE && pref != BoolPreference.FALSE) {
-        localStorage.setItem(name, defaultValue);
-        pref = defaultValue;
-    }
-
-    document.getElementById(name).checked = pref == BoolPreference.TRUE;
+function setupBoolPreference(name) {
+    document.getElementById(name).checked = Preferences[name].value;
 
     document.getElementById(name).addEventListener("change", function(e) {
-        if(e.target.checked) {
-            localStorage.setItem(name, BoolPreference.TRUE);
-        }
-        else {
-            localStorage.setItem(name, BoolPreference.FALSE);
-        }
+        Preferences[name].value = e.target.checked;
     });
-}
+};
 
-BoolPreference("vibration", BoolPreference.TRUE);
-BoolPreference("autotoggle", BoolPreference.FALSE);
+for(var name in Preferences) {
+    setupBoolPreference(name);
+}
 
 document.querySelector("[data-l10n-id='settings_highscores_clear']").addEventListener("click", function() {
     if(window.confirm(document.querySelector("[data-l10n-id='highscores_confirm_clear']").textContent)) {
