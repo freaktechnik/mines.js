f1459665d40c499e728d51f12bf76dc163087b06
Martin Giger <martin@humanoids.be>
2015-10-19 20:21:56 +0200

    Fix the html validity

--- assets/scripts/licenses-content.js
+++ assets/scripts/licenses-content.js
@@ -3,7 +3,7 @@ document.getElementById("header").addEventListener("action", function() {
 }, false);
 
 var links = document.querySelector('[role="main"]').addEventListener("click", function(e) {
-    if(e.target.tagName.toLowerCase() === "a" && e.target.rel != "source") {
+    if(e.target.tagName.toLowerCase() === "a" && e.target.rel != "external") {
         window.location = "source-viewer.html#" + e.target.href;
         e.preventDefault();
     }
