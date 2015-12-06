bd116f2a36966120cb4d6c34f38499e62d69ec8b
Martin Giger <martin@humanoids.be>
2015-12-06 20:13:00 +0100

    Add percentage display on custom game creation screen

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,8 +1,9 @@
 var mines = document.getElementById("mines"),
     height = document.getElementById("height"),
     width = document.getElementById("width"),
+    percentage = document.getElementById("percentage"),
     strbundle = new StringBundle(document.getElementById("strings")),
-    minesValidator = function(e) {
+    minesValidator = function() {
         var cells = height.valueAsNumber * width.valueAsNumber;
         if(mines.valueAsNumber >= cells || cells < 2) {
             mines.setCustomValidity(strbundle.getString('custom_form_error'));
@@ -10,10 +11,39 @@ var mines = document.getElementById("mines"),
         else {
             mines.setCustomValidity("");
         }
+    },
+    toFixed = function(number) {
+        if(nf)
+            return nf.format(number);
+        else
+            return number.toFixed(1);
+    },
+    updateOutput = function() {
+        if(mines.value.length && height.value.length && width.value.length) {
+            strbundle.getStringAsync(
+                "custom_percentage_unit",
+                {
+                    percentage: toFixed(mines.valueAsNumber / (height.valueAsNumber * width.valueAsNumber) * 100)
+                }
+            ).then(function(val) {
+                percentage.value = val;
+            });
+        }
+    },
+    listener = function() {
+        minesValidator();
+        updateOutput();
     };
 
-mines.addEventListener("change", minesValidator, false);
-mines.addEventListener("input", minesValidator, false);
+if("Intl" in window)
+    var nf = new Intl.NumberFormat(undefined, { maximumFractionDigits: 1, minimumFractionDigits: 0 });
+
+mines.addEventListener("change", listener, false);
+mines.addEventListener("input", listener, false);
+height.addEventListener("change", listener, false);
+height.addEventListener("input", listener, false);
+width.addEventListener("change", listener, false);
+width.addEventListener("input", listener, false);
 mines.addEventListener("invalid", minesValidator, false);
 
 document.getElementById("form").addEventListener("submit", function(e) {
@@ -32,3 +62,5 @@ document.getElementById("submit").addEventListener("click", function(e) {
 document.getElementById("header").addEventListener("action", function(e) {
     document.location = "index.html";
 }, false);
+
+updateOutput();
