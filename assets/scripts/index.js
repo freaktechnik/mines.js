e845084c88cc656d03d0c9cd69027197eabe4348
Martin Giger <martin@humanoids.be>
2016-01-09 14:10:12 +0100

    Deprecating marketplace :S

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,13 +2,3 @@ if(Mines.hasSavedState()) {
     document.getElementById("resumeSection").classList.remove("hidden");
     document.getElementById("resumeSection").removeAttribute("hidden");
 }
-
-// Show marketplace promo if we're in a Firefox that supports webapps
-if(document.getElementById("marketplacepromo") && "mozApps" in navigator) {
-    navigator.mozApps.getSelf().then(function(res) {
-        // make sure we're not an installed hosted webapp
-        if(res === null) {
-            document.getElementById("marketplacepromo").removeAttribute("hidden");
-        }
-    });
-}
