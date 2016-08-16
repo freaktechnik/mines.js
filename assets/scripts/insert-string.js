783c4cd7ce12d4932d9554f27d616d6818865868
Martin Giger <martin@humanoids.be>
2016-08-16 12:10:50 +0200

    Add missing file

---
+++ assets/scripts/insert-string.js
@@ -0,0 +1,43 @@
+const attrMap = {
+        "aria-label": "ariaLabel"
+    },
+    translateElement = (bundle, id, options, attr, simple = false) => {
+        if(simple) {
+            return bundle[id];
+        }
+
+        let str = ` data-l10n-id="${id}">`,
+            string = bundle[id] || bundle[`${id}.innerHTML`];
+        if(attr && !(attr in attrMap)) {
+            string = bundle[id + '.' + attr];
+        }
+        else if(attr && attr in attrMap) {
+            string = bundle[id + '.' + attrMap[attr]];
+        }
+
+        if(!string || !string.trim().length) {
+            throw `Empty string ${id}`;
+        }
+
+        if(options) {
+            for(const opt in options) {
+                string = string.replace(new RegExp(`{{${opt}}}`, "g"), options[opt]);
+            }
+            str = ` data-l10n-args='${JSON.stringify(options)}'${str}`;
+        }
+
+        if(!attr) {
+            str += string;
+        }
+        else {
+            str = ` ${attr}="${string}"${str}`;
+        }
+
+        return str;
+    };
+
+export default (defaultLanguage) => {
+    const bundle = require(`!!properties!../../locales/${defaultLanguage}/app.properties`);
+
+    return translateElement.bind(null, bundle);
+};
