40a0361daed8c83c7b77d4db5f3c131ccda9f110
Martin Giger <martin@humanoids.be>
2018-03-24 22:04:18 +0100

    chore: some solver stuff

--- src/mines/grid-solver/index.js
+++ src/mines/grid-solver/index.js
@@ -1,5 +1,7 @@
 import { NotImplemented } from '../error';
 
+const NONE = -1;
+
 //TODO make this workerable?
 /**
  * @interface
@@ -15,6 +17,22 @@ export default class GridSolver {
      * @returns {boolean} Whether the attempt to solve succeeded.
      */
     attemptToSolve() {
-        throw new NotImplemented("attemptToSolve");
+        let didSolve = true;
+        let solved = NONE;
+
+        while(didSolve && solved !== this.grid.cellNumber) {
+            didSolve = this.solveStep();
+            solved = this.grid.countCompleteCells();
+        }
+        return solved === this.grid.cellNumber;
+    }
+
+    /**
+     * Tries to solve one cell in the given grid.
+     *
+     * @returns {boolean} Whether it succeeded to solve one cell.
+     */
+    solveStep() {
+        throw new NotImplemented("solveStep");
     }
 }
--- src/mines/grid-solver/perfect-solver.js
+++ src/mines/grid-solver/perfect-solver.js
@@ -1,42 +1,30 @@
 import GridSolver from '.';
 
-const NONE = -1;
-
 /**
- * @implements module:mines/grid-solver
  * @exports module:mines/grid-solver/perfect-solver
  */
 export default class PerfectSolver extends GridSolver {
-    attemptToSolve() {
-        let completed = this.grid.countCompleteCells(),
-            prevCompleted = NONE;
-
-        //TODO will need adjustment if solved is true even when not everythong's flagged.
-
-        while(!this.grid.solved && completed != prevCompleted) {
-            for(const cell of this.grid.cells) {
-                if(cell.covered && cell.fullySurrounded) {
-                    //TODO flag or uncover cell or happens via other steps?
+    solveStep() {
+        for(const cell of this.grid.cells) {
+            if(cell.covered && cell.fullySurrounded) {
+                //TODO flag or uncover cell or happens via other steps?
+            }
+            else if(cell.fullyUncovered && !cell.fullyMarked) {
+                cell.markNeighbours();
+            }
+            else if(!cell.covered && !cell.fullySurrounded) {
+                try {
+                    cell.uncover();
                 }
-                else if(cell.fullyUncovered && !cell.fullyMarked) {
-                    cell.markNeighbours();
+                catch(e) {
+                    return false;
                 }
-                else if(!cell.covered && !cell.fullySurrounded) {
-                    try {
-                        cell.uncover();
-                    }
-                    catch(e) {
-                        return false;
-                    }
-                }
-
-                //TODO other patterns like 111 and 121.
-                //TODO uncover cells if all missing mines are somewhere on the border of the uncovered part of the grid.
             }
 
-            prevCompleted = completed;
-            completed = this.grid.countCompleteCells();
+            //TODO other patterns like 111 and 121.
+            //TODO uncover cells if all missing mines are somewhere on the border of the uncovered part of the grid.
         }
-        return completed != prevCompleted;
+        //TODO return true sometimes.
+        return false;
     }
 }
